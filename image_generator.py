f906f7a49e85eb2d896fef2a1a2d66fe1155b450
Nancy Xing <92757073+ncy-xing@users.noreply.github.com>
2023-09-09 20:44:13 -0400

    vertical bands and single color overlay

--- image_generator.py
+++ image_generator.py
@@ -3,27 +3,43 @@ import PIL
 import numpy as np
 import os.path
 
-class ImageGenerator():
-    def __init__(self, color_matrix=None):
-        self.color_matrix = color_matrix
-        self.colors = list(color_matrix.keys())
 
-    def generate_image(self, filename, height=100, width=100, start_pixel="#000000"):
+
+class ImageGenerator():
+
+    def __init__(self, filename=None):
+        self.filename = filename
+        self.image = Image.open(os.path.join("input", self.filename))
+        self.width, self.height = self.image.size
+        self.color_matrix = self.generate_matrix()
+        self.colors = list(self.color_matrix.keys())
+
+    def generate_image(self):
         '''
         Generates an image given a filename, dimensions, and transition matrix of RBG values probability of the following colors.
         '''
-        image = Image.new(mode="RGB", size=(width, height))
-        pixel_map = image.load()
-        current_pixel = start_pixel
+        starting_color = np.random.choice(list(self.color_matrix.keys()))
 
-        for w in range(width):
-            for h in range(height):
-                next_pixel = self.get_next_pixel(current_pixel)
-                # Convert hex to RGB and add as a pixel to the image
-                pixel_map[w, h] = ImageColor.getcolor(next_pixel, "RGB")
+        image = Image.new(mode="RGB", size=(self.width, self.height))
+        pixel_map = image.load()
+        bw_map = self.image.convert('1', dither=Image.NONE).load()
+        lesser_color = self.lesser_color(bw_map)
+        current_pixel = starting_color
+
+        # Generate colors from matrix
+        for w in range(image.width):
+            next_pixel = self.get_next_pixel(current_pixel)
+            # Convert hex to RGB and add as a pixel to the image
+            next_color = ImageColor.getcolor(next_pixel, "RGB")    
+            for h in range(image.height):
+                if bw_map[w,h] == lesser_color:
+                    pixel_map[w, h] = (255, 255, 255)
+                else:
+                    pixel_map[w, h] = next_color
                 current_pixel = next_pixel
         
-        image.save(os.path.join("output", filename), format="png")
+        # Apply white dither
+        image.save(os.path.join("output", "generated_" + self.filename), format="png")
     
     def get_next_pixel(self, current_pixel):
         '''
@@ -40,56 +56,56 @@ class ImageGenerator():
             self.colors, p=probabilities
             )
 
-def generate_matrix(filename):
-    '''
-    Given the filename of an image, reduces the number of colors in image and converts into a transition matrix. 
-    '''
-    image = Image.open(os.path.join("input", filename))
-    # Reduce the amount of colors in matrix
-    image = image.quantize(colors=50).convert('RGB')
+    def generate_matrix(self):
+        '''
+        Given the filename of an image, reduces the number of colors in image and converts into a transition matrix. 
+        '''
+        
+        # Reduce the amount of colors in matrix
+        reduced_image = self.image.quantize(colors=50).convert('RGB')
 
-    width, height = image.size
-    matrix = {}
-    current_hex = '#%02x%02x%02x' % image.getpixel((0, 0))
+        matrix = {}
+        current_hex = '#%02x%02x%02x' % reduced_image.getpixel((0, 0))
 
-    # Get counts of every color
-    for w in range(1, width):
-        for h in range(1, height):
-            r, g, b = image.getpixel((w, h))
-            next_hex = '#%02x%02x%02x' % (r, g, b)
+        # Get counts of every color
+        for w in range(1, self.width):
+            for h in range(1, self.height):
+                r, g, b = reduced_image.getpixel((w, h))
+                next_hex = '#%02x%02x%02x' % (r, g, b)
 
-            if matrix.get(current_hex):
-            # Update if current color exists
-                if matrix[current_hex].get(next_hex):
-                    matrix[current_hex][next_hex] += 1
+                if matrix.get(current_hex):
+                # Update if current color exists
+                    if matrix[current_hex].get(next_hex):
+                        matrix[current_hex][next_hex] += 1
+                    else:
+                        matrix[current_hex].update({next_hex : 1})
+                # Add new color as key to matrix
                 else:
-                    matrix[current_hex].update({next_hex : 1})
-            # Add new color as key to matrix
-            else:
-                matrix.update({current_hex : {next_hex : 1}})
+                    matrix.update({current_hex : {next_hex : 1}})
 
-            current_hex = next_hex
+                current_hex = next_hex
 
-    # Convert next color counts into probabilities 
-    for hex, counts in matrix.items():
-        total_counts = sum(counts.values())
-        for next_color, c in counts.items():
-            matrix[hex].update({next_color : c / total_counts})
+        # Convert next color counts into probabilities 
+        for hex, counts in matrix.items():
+            total_counts = sum(counts.values())
+            for next_color, c in counts.items():
+                matrix[hex].update({next_color : c / total_counts})
 
-    return matrix
+        return matrix
+    
+    def lesser_color(self, bw_map):
+        white_count = 0
+        for w in range(self.image.width):
+            for h in range(self.image.height):
+                if bw_map[w, h] == 255:
+                    white_count += 1
+        return 255 if white_count <= (self.image.height * self.image.width) / 2 else 0
 
 if __name__ == "__main__":
-
-    matrix = generate_matrix("grumbot.png")
-    gen = ImageGenerator(color_matrix=matrix)
-    # Set a random starting color
-    starting_color = np.random.choice(list(matrix.keys()))
-    gen.generate_image("grumbot-test.png", 100, 100, starting_color)
-
-    # Matrix format
-    # matrix = {
-    #     "000000" : {"000000" : 0.7, "FFFFFF" : 0.2, "FF00FF" : 0.1},
-    #     "FFFFFF" : {"000000" : 0.7, "FFFFFF" : 0.2, "FF00FF" : 0.1},
-    #     "FF00FF" : {"000000" : 0.7, "FFFFFF" : 0.2, "FF00FF" : 0.1}
-    # }
-
+    
+    print("Generating images...")
+    for filename in os.listdir("input"):
+        if os.path.splitext(filename)[1] in [".jpg", ".png"]:
+            gen = ImageGenerator(filename)
+            gen.generate_image()
+    print("Image generation complete.")
